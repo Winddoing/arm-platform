baeae6cc5834c1c09022c4cc067b59db80b60113
wqshao <shaowangquan@gmail.com>
2016-09-17 15:59:48 +0800

    add arm-linux-gcc for Cortex-A8

--- envsetup.sh
+++ envsetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PATH=$PATH:/home/wqshao/arm_platform/arm_platform/toolchain/FriendlyARM/toolschain/4.4.3/bin
+MYPWD=`pwd`
+PATH=$PATH:$MYPWD/toolchain/arm-2014.05/bin
 
 export set PATH
 echo $PATH
---
+++ toolchain/README.md
@@ -0,0 +1,11 @@
+
+## arm 交叉编译工具链
+
+使用方法：
+
+1.解压到当前目录
+
+    tar jxvf arm-2014.05-29-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+
+2.添加arm-none-linux到envsetup
+
